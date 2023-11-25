6ca982a7594a5a88c2c4a58ab4c4b7d8e41310a7
Remon Tripura <remontripura045@gmail.com>
2023-11-25 06:38:12 +0600

    crating schema

--- src/app/modules/users/user.interface.ts
+++ src/app/modules/users/user.interface.ts
@@ -1,24 +1,24 @@
-interface Order {
-    productName: string;
-    price: number;
-    quantity: number;
-}
+export type Order = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
 
-interface User {
-    userId: string;
-    password: string;
-    fullname: {
-        firstName: string;
-        lastName: string;
-    };
-    age: number;
-    email: string;
-    isActive: boolean;
-    hobbies: string[];
-    address: {
-        street: string;
-        city: string;
-        country: string
-    };
-    orders: Order[];
-  }
+export type User = {
+  userId: string;
+  password: string;
+  fullname: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: {
+    street: string;
+    city: string;
+    country: string;
+  };
+  orders: Order[];
+};
